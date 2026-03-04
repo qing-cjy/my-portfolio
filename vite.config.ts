6a5b25d1bc9320f63969677cdcd94bf7e0c668e8
qingli <chengjiayiheqingli@qq.com>
2026-03-04 23:52:30 +0800

    fix: upgrade to tailwind v4 and fix postcss build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 1. 导入插件
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),      // 2. 保留原有的 React 插件
+    tailwindcss(), // 3. 添加 Tailwind 插件
+  ],
 })
