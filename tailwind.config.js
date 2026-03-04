0e9f4b3aa0c669d70e308c48a31cb35ece0930c8
qingli <chengjiayiheqingli@qq.com>
2026-03-04 23:11:33 +0800

    fix: 修复 Tailwind CSS 构建配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,12 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+  // 强制指定 PostCSS 解析器，解决 Vercel 报错
+  css: {
+    postcss: true,
+  },
 }
